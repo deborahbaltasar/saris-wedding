a4f4cab349112d714a9232c1f6dbe8a25e5e9809
unknown <deborahbnogueira.dn@gmail.com>
2025-09-04 19:17:46 -0300

    ci: adjust vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -49,9 +49,10 @@
         '@': path.resolve(__dirname, './src'),
       },
     },
+    base: '/', 
     build: {
       target: 'esnext',
-      outDir: 'build',
+      outDir: 'dist',
     },
     server: {
       port: 3000,
